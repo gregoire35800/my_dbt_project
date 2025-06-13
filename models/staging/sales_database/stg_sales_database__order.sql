90dba6a68e82affe8ecab1f87cf05a32e2d149a0
Grégoire Le Gac <gregoire.le-gac@cooperl.com>
2025-06-13 11:23:11 +0200

    modification post merge

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -2,17 +2,9 @@ select
     order_id,
     user_name as user_id,
     order_status,
-<<<<<<< HEAD
     DATETIME(order_date, "Europe/Paris") AS order_created_at_y,
     DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_y,
     DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_y,
     DATETIME(delivered_date, "Europe/Paris") AS delivered_at_y,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery
-=======
-    DATETIME(order_date, "Europe/Paris") AS order_created_at,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_at,
     DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_x
->>>>>>> origin/main
 from {{ source('sales_database', 'order') }}
