c0577e1a1e2a2fcd9dcb172722d029c70133717f
Grégoire Le Gac <gregoire.le-gac@cooperl.com>
2025-06-13 11:28:22 +0200

    my second changement

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -6,5 +6,5 @@ select
     DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_y,
     DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_y,
     DATETIME(delivered_date, "Europe/Paris") AS delivered_at_y,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_x
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_xy
 from {{ source('sales_database', 'order') }}
